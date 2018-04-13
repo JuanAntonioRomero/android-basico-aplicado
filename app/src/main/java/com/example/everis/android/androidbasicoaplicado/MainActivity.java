5604610623cb3dce4406c0ccfc336f0e73a93900
Juan Antonio Romero Gómez <juan.antonio.romero.gomez@everis.com>
2018-04-13 14:59:21 +0200

    Fix initial project

--- app/src/main/java/com/example/everis/android/androidbasicoaplicado/MainActivity.java
+++ app/src/main/java/com/example/everis/android/androidbasicoaplicado/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.everis.android.androidbsicoaplicado;
+package com.example.everis.android.androidbasicoaplicado;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
