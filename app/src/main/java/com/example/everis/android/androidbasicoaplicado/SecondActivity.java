1457a21945fba930388f261d49e89178af5ad7df
Juan Antonio Romero Gómez <juan.antonio.romero.gomez@everis.com>
2018-04-14 14:21:25 +0200

    Transitions sample

---
+++ app/src/main/java/com/example/everis/android/androidbasicoaplicado/SecondActivity.java
@@ -0,0 +1,19 @@
+package com.example.everis.android.androidbasicoaplicado;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.enter2, R.anim.exit2);
+    }
+}
