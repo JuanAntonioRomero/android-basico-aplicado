9dbf0b011202f69da3952eb6882827818bb4c587
Juan Antonio Romero Gómez <juan.antonio.romero.gomez@everis.com>
2018-04-14 13:51:53 +0200

    Web service call sample

---
+++ app/src/main/java/com/example/everis/android/androidbasicoaplicado/Api.java
@@ -0,0 +1,149 @@
+package com.example.everis.android.androidbasicoaplicado;
+
+import android.os.AsyncTask;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Api {
+
+    public interface ApiCallListener {
+        void success(String response);
+    }
+
+    public static class ApiCallPost extends AsyncTask<String, String, String> {
+
+        private ApiCallListener mListener;
+
+        public ApiCallPost(ApiCallListener listener) {
+            mListener = listener;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String res = null;
+
+            String urlString = params[0]; // URL to call
+            String data = params[1]; //data to post
+
+            OutputStream out = null;
+            try {
+                URL url = new URL(urlString);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setDoOutput(true);
+                urlConnection.setDoInput(true);
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+                urlConnection.setRequestProperty("Accept", "application/json");
+                urlConnection.setRequestMethod("POST");
+
+                out = new BufferedOutputStream(urlConnection.getOutputStream());
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, "UTF-8"));
+                writer.write(data);
+                writer.flush();
+                writer.close();
+                out.close();
+                urlConnection.connect();
+                res = readInputStreamToString(urlConnection);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+            return res;
+        }
+
+        @Override
+        protected void onPostExecute(String response) {
+            super.onPostExecute(response);
+
+            if (mListener != null) {
+                mListener.success(response);
+            }
+        }
+    }
+
+    public static class ApiCallGet extends AsyncTask<String, String, String> {
+        private ApiCallListener mListener;
+
+        public ApiCallGet(ApiCallListener listener) {
+            mListener = listener;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String res = null;
+            String urlString = params[0]; // URL to call
+
+            OutputStream out = null;
+            try {
+                URL url = new URL(urlString);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+                urlConnection.setRequestProperty("Accept", "application/json");
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+                res = readInputStreamToString(urlConnection);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+            return res;
+        }
+
+        @Override
+        protected void onPostExecute(String response) {
+            super.onPostExecute(response);
+
+            if (mListener != null) {
+                mListener.success(response);
+            }
+        }
+    }
+
+    private static String readInputStreamToString(HttpURLConnection connection) {
+        String result;
+        StringBuffer sb = new StringBuffer();
+        InputStream is = null;
+
+        try {
+            is = new BufferedInputStream(connection.getInputStream());
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String inputLine = "";
+            while ((inputLine = br.readLine()) != null) {
+                sb.append(inputLine);
+            }
+            result = sb.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = null;
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return result;
+    }
+}
--- app/src/main/java/com/example/everis/android/androidbasicoaplicado/MainActivity.java
+++ app/src/main/java/com/example/everis/android/androidbasicoaplicado/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.everis.android.androidbasicoaplicado;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String GET_URL = "https://jsonplaceholder.typicode.com/posts/1";
+    private static final String POST_URL = "https://jsonplaceholder.typicode.com/posts";
+    private static final String POST_BODY = "{\"title\":\"foo\",\"body\": \"bar\",\"userId\": 1}";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        final TextView textView = findViewById(R.id.text);
+
+        findViewById(R.id.button_get).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Api.ApiCallGet(new Api.ApiCallListener() {
+                    @Override
+                    public void success(String response) {
+                        textView.setText(response);
+                    }
+                }).execute(new String[]{GET_URL});
+            }
+        });
+
+        findViewById(R.id.button_post).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new Api.ApiCallPost(new Api.ApiCallListener() {
+                    @Override
+                    public void success(String response) {
+                        textView.setText(response);
+                    }
+                }).execute(new String[]{POST_URL, POST_BODY});
+            }
+        });
     }
 }
