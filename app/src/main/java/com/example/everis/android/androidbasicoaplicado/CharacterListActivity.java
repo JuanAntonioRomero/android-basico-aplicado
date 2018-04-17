c3ce94114462b65793987a583b89276f9a2127dd
Juan Antonio Romero Gómez <juan.antonio.romero.gomez@everis.com>
2018-04-17 09:40:19 +0200

    Initial classes

---
+++ app/src/main/java/com/example/everis/android/androidbasicoaplicado/CharacterListActivity.java
@@ -0,0 +1,13 @@
+package com.example.everis.android.androidbasicoaplicado;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CharacterListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character_list);
+    }
+}
--- app/src/main/java/com/example/everis/android/androidbasicoaplicado/LetterListActivity.java
+++ app/src/main/java/com/example/everis/android/androidbasicoaplicado/LetterListActivity.java
@@ -3,11 +3,11 @@ package com.example.everis.android.androidbasicoaplicado;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class LetterListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_letter_list);
     }
 }
